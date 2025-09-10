c8518fc08fabcc0685a72233fa9f61004738a3b8
Felix <chenwei.wan@wustl.edu>
2025-09-10 16:57:15 -0500

    j

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Ruin {
+public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+System.out.println("Total Simulations: ");
+int totalSimulations = in.nextInt();
+System.out.println("Start Amount: ");
+int startAmount = in.nextInt();
+System.out.println("Win Chance: ");
+double winChance = in.nextDouble();
+System.out.println("Win Limit: ");
+int winLimit = in.nextInt();
+
+
+for (int i = totalSimulations; i>0; i--){
+    int currentAmount = startAmount;
+while (currentAmount>0 || currentAmount<=winLimit){
+    if (Math.random() < winChance){
+        currentAmount++; 
+    } else {
+        currentAmount--; 
+    } 
+    
+}
+    System.out.println(currentAmount);
+    if (currentAmount<winLimit){
+        System.out.println("ruin");
+    }
+    else{
+        System.out.println("success");
+    }
+    
+
+}
+}
+}
